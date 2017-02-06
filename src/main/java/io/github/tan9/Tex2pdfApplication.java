89b99358512478372a4352cdad71e569d4ec3ae9
Pei-Tang Huang <tangtheone@gmail.com>
2017-02-06 23:24:29 +0800

    Init commit from Spring Initializr.

---
+++ src/main/java/io/github/tan9/Tex2pdfApplication.java
@@ -0,0 +1,12 @@
+package io.github.tan9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tex2pdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tex2pdfApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/tan9/Tex2pdfApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.tan9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tex2pdfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
