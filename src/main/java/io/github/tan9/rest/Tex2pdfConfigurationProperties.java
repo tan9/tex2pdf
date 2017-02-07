2702e026e38497d3fccfaf84e156018951ea08e7
Pei-Tang Huang <tangtheone@gmail.com>
2017-02-08 00:24:24 +0800

    Refactor.

--- src/main/java/io/github/tan9/rest/Tex2pdfConfigurationProperties.java
+++ src/main/java/io/github/tan9/rest/Tex2pdfConfigurationProperties.java
@@ -12,4 +12,8 @@ public class Tex2pdfConfigurationProperties {
 
     private Path workDirectory;
 
+    private String texliveImage = "listx/texlive:2016-fonts";
+
+    private String texCommand = "xelatex";
+
 }
--- src/main/java/io/github/tan9/rest/Tex2pdfResource.java
+++ src/main/java/io/github/tan9/rest/Tex2pdfResource.java
@@ -1,6 +1,7 @@
 package io.github.tan9.rest;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -33,6 +34,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Tex2pdfResource {
 
+    private static final String TEMP_TEX_FILENAME = "temp.tex";
+    private static final String TEMP_PDF_FILENAME = "temp.pdf";
+
     @Autowired
     private Tex2pdfConfigurationProperties properties;
 
@@ -41,51 +45,70 @@ public class Tex2pdfResource {
     @ResponseBody
     ResponseEntity<?> tex2pdf(@RequestBody @Validated TexRequest request) {
         try {
-            String texContent = request.getTexContent();
-
             Path tempDirectory = Files.createTempDirectory(properties.getWorkDirectory(), "tex2pdf");
             try {
-                Path tempFile = Paths.get(tempDirectory.toString(), "temp.tex");
-                Files.createFile(tempFile);
-                Files.write(tempFile, texContent.getBytes());
-
-                DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-                List<Container> containers = dockerClient.listContainersCmd().exec().stream().filter(container ->
-                        "listx/texlive:2016-fonts".equals(container
-                                .getImage())).collect(Collectors.toList());
-
-                if (containers.size() != 1) {
-                    String message = "Expected exact one texlive container, but found: " + containers;
-                    throw new IllegalStateException(message);
-                }
-
-                ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(containers.iterator().next().getId())
-                        .withAttachStdout(true).withCmd("sh", "-c", "cd '" + tempDirectory
-                                + "' && xelatex temp.tex").exec();
-
-                dockerClient.execStartCmd(execCreateCmdResponse.getId()).exec(
-                        new ExecStartResultCallback(System.out, System.err)).awaitCompletion(1, TimeUnit.MINUTES);
-
-                byte[] pdfBytes = Files.readAllBytes(tempDirectory.resolve("temp.pdf"));
-                String encodedPdfContent = Base64.getEncoder().encodeToString(pdfBytes);
-
-                PdfResponse response = new PdfResponse();
-                response.setEncodedPdfContent(encodedPdfContent);
-
-                return ResponseEntity.ok(response);
+                writeTempTexFile(tempDirectory, TEMP_TEX_FILENAME, request.getTexContent());
+                performTex2pdf(tempDirectory, TEMP_TEX_FILENAME);
+                return ResponseEntity.ok(createPdfResponse(tempDirectory, TEMP_PDF_FILENAME));
 
             } finally {
-                if (tempDirectory != null) {
-                    Files.walk(tempDirectory, FileVisitOption.FOLLOW_LINKS)
-                            .sorted(Comparator.reverseOrder())
-                            .map(Path::toFile)
-                            .forEach(File::delete);
-                }
+                Files.walk(tempDirectory, FileVisitOption.FOLLOW_LINKS)
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
             }
 
         } catch (Exception ex) {
             log.error("Failed to process request.", ex);
             return ResponseEntity.unprocessableEntity().body(ex.getMessage());
         }
+    }
+
+    private void writeTempTexFile(Path tempDirectory, String texFilename, String texContent) throws IOException {
+        Path tempFile = Paths.get(tempDirectory.toString(), texFilename);
+        Files.createFile(tempFile);
+        Files.write(tempFile, texContent.getBytes());
+    }
+
+    private void performTex2pdf(Path tempDirectory, String texFilename) throws InterruptedException {
+        DockerClient dockerClient = getDockerClient();
+        Container container = getTexLiveContainer(dockerClient);
+
+        String command = "cd '" + tempDirectory + "' && " + properties.getTexCommand() + " " + texFilename;
+        ExecCreateCmdResponse execCreateCmdResponse = dockerClient
+                .execCreateCmd(container.getId())
+                .withAttachStdout(true)
+                .withAttachStderr(true)
+                .withCmd("sh", "-c", command).exec();
+
+        dockerClient.execStartCmd(execCreateCmdResponse.getId())
+                .exec(new ExecStartResultCallback(System.out, System.err))
+                .awaitCompletion(1, TimeUnit.MINUTES);
+    }
+
+    private DockerClient getDockerClient() {
+        return DockerClientBuilder.getInstance().build();
+    }
+
+    private Container getTexLiveContainer(DockerClient dockerClient) {
+        List<Container> containers = dockerClient.listContainersCmd().exec().stream()
+                .filter(container -> properties.getTexliveImage().equals(container.getImage()))
+                .collect(Collectors.toList());
+
+        if (containers.size() != 1) {
+            String message = "Expected exact one TeX Live container of image: " + properties.getTexliveImage() + ". " +
+                    "But found: " + containers;
+            throw new IllegalStateException(message);
+        }
+        return containers.iterator().next();
+    }
+
+    private PdfResponse createPdfResponse(Path tempDirectory, String pdfFilename) throws IOException {
+        byte[] pdfBytes = Files.readAllBytes(tempDirectory.resolve(pdfFilename));
+        String encodedPdfContent = Base64.getEncoder().encodeToString(pdfBytes);
+
+        PdfResponse response = new PdfResponse();
+        response.setEncodedPdfContent(encodedPdfContent);
+        return response;
     }
 }
